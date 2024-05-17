8ffa435ec976450b79153038138be88dd179088f
Thaihoangbach <120196976+Thaihoangbach@users.noreply.github.com>
2024-05-17 14:44:12 +0700

    22022672

--- Library_Management/main.py
+++ Library_Management/main.py
@@ -40,19 +40,19 @@ button_bg_color = "#8B4513"  # Dark brown color
 button_fg_color = "white"
 
 # Add buttons on top of the background image
-btn1 = Button(root, text="Add Book Details", bg=button_bg_color, fg=button_fg_color, font=('Times New Roman', 15), command=addBook)
+btn1 = Button(root, text="Add Book Details", bg=button_bg_color, fg=button_fg_color, font=('Times New Roman', 13), command=addBook)
 btn1.place(relx=0.33, rely=0.38, relwidth=0.35, relheight=0.1)
 
-btn2 = Button(root, text="Delete Book", bg=button_bg_color, fg=button_fg_color, font=('Times New Roman', 15), command=delete)
+btn2 = Button(root, text="Delete Book", bg=button_bg_color, fg=button_fg_color, font=('Times New Roman', 13), command=delete)
 btn2.place(relx=0.33, rely=0.49, relwidth=0.35, relheight=0.1)
 
-btn3 = Button(root, text="View Book List", bg=button_bg_color, fg=button_fg_color, font=('Times New Roman', 15), command=View)
+btn3 = Button(root, text="View Book List", bg=button_bg_color, fg=button_fg_color, font=('Times New Roman', 13), command=View)
 btn3.place(relx=0.33, rely=0.6, relwidth=0.35, relheight=0.1)
 
-btn4 = Button(root, text="Issue Book to Student", bg=button_bg_color, fg=button_fg_color, font=('Times New Roman', 15), command=issueBook)
+btn4 = Button(root, text="Issue Book to Student", bg=button_bg_color, fg=button_fg_color, font=('Times New Roman', 13), command=issueBook)
 btn4.place(relx=0.33, rely=0.71, relwidth=0.35, relheight=0.1)
 
-btn5 = Button(root, text="Return Book", bg=button_bg_color, fg=button_fg_color, font=('Times New Roman', 15), command=returnBook)
+btn5 = Button(root, text="Return Book", bg=button_bg_color, fg=button_fg_color, font=('Times New Roman', 13), command=returnBook)
 btn5.place(relx=0.33, rely=0.82, relwidth=0.35, relheight=0.1)
 
 root.mainloop()
