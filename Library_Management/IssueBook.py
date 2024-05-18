3c28acd9be3bccdf0ce6153ef7176dce1014b486
NamPham2124 <168910794+NamPham2124@users.noreply.github.com>
2024-05-18 22:17:33 +0700

    Update IssueBook.py

--- Library_Management/IssueBook.py
+++ Library_Management/IssueBook.py
@@ -1,14 +1,81 @@
 from tkinter import *
 from PIL import ImageTk,Image
 from tkinter import messagebox
-import pymysql
+from connect_database import connect_to_database
 
+
+# Enter Table Names here
+issueTable = "books_issued" 
+bookTable = "books"
+    
+#List To store all Book IDs
+allBid = [] 
 
 def issue():
+    con, cur = connect_to_database()
+    global issueBtn,labelFrame,lb1,inf1,inf2,quitBtn,root,Canvas1,status
     
-    pass
+    bid = inf1.get()
+    issueto = inf2.get()
+
+    issueBtn.destroy()
+    labelFrame.destroy()
+    lb1.destroy()
+    inf1.destroy()
+    inf2.destroy()
+    
+    
+    extractBid = "select bid from "+bookTable
+    try:
+        cur.execute(extractBid)
+        con.commit()
+        for i in cur:
+            allBid.append(i[0])
+        
+        if bid in allBid:
+            checkAvail = "select status from "+bookTable+" where bid = '"+bid+"'"
+            cur.execute(checkAvail)
+            con.commit()
+            for i in cur:
+                check = i[0]
+                
+            if check == 'avail':
+                status = True
+            else:
+                status = False
+
+        else:
+            messagebox.showinfo("Error","Book ID not present")
+    except:
+        messagebox.showinfo("Error","Can't fetch Book IDs")
+    
+    issueSql = "insert into "+issueTable+" values ('"+bid+"','"+issueto+"')"
+    show = "select * from "+issueTable
+    
+    updateStatus = "update "+bookTable+" set status = 'issued' where bid = '"+bid+"'"
+    try:
+        if bid in allBid and status == True:
+            cur.execute(issueSql)
+            con.commit()
+            cur.execute(updateStatus)
+            con.commit()
+            messagebox.showinfo('Success',"Book Issued Successfully")
+            root.destroy()
+        else:
+            allBid.clear()
+            messagebox.showinfo('Message',"Book Already Issued")
+            root.destroy()
+            return
+    except:
+        messagebox.showinfo("Search Error","The value entered is wrong, Try again")
+    
+    print(bid)
+    print(issueto)
+    
+    allBid.clear()
     
 def issueBook(): 
+    
     global issueBtn,labelFrame,lb1,inf1,inf2,quitBtn,root,Canvas1,status
     
     root = Tk()
@@ -23,7 +90,7 @@ def issueBook():
     headingFrame1 = Frame(root,bg="#FFBB00",bd=5)
     headingFrame1.place(relx=0.25,rely=0.1,relwidth=0.5,relheight=0.13)
         
-    headingLabel = Label(headingFrame1, text="Issue Book", bg='black', fg='white', font=('Times New Roman',15))
+    headingLabel = Label(headingFrame1, text="Issue Book", bg='black', fg='white', font=('Courier',15))
     headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
     
     labelFrame = Frame(root,bg='black')
